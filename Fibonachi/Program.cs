d238f5ce5b737a40288b25f082a3e3f815060e54
AlgirdasGurskis <algis_genesis@yahoo.com>
2020-08-04 22:54:23 +0300

    20200526-AGU

---
+++ Fibonachi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System; 
+
+namespace Fibonachi
+{
+    class Program
+    {
+        static int p1 = 0, p2 = 1, p3;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some number to stop the fibonacci sequence.");
+            int num = int.Parse(Console.ReadLine());
+            int first = 1;
+            Console.Write("{0},", first);
+            int second = 1;
+            Console.Write("{0},", first);
+            int sum = 0;
+
+            while (sum <= num)
+            {
+                sum = first + second;
+                 Console.Write("{0},", sum);
+
+                first = second;
+                second = sum;
+            }
+            Console.WriteLine("\n\n");
+            Fibonacci(num - 2);
+            Console.WriteLine("\n\nIt done, please enter any key!");
+            Console.ReadKey();
+        }
+        private static void Fibonacci(int count)
+        {
+            if (count>0)
+            {
+                p3 = p1 + p2;
+                Console.Write("  " + p3);
+                p1 = p2;
+                p2 = p3;
+                Fibonacci(count - 1);
+            }
+        }
+    }
+}
